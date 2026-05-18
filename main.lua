215069e7e81fc774fd8f071090b648c1c5626118
BjornVis010 <bjornvis010@gmail.com>
2026-05-19 00:02:53 +0200

    making a start

--- main.lua
+++ main.lua
@@ -1,1 +1,18 @@
-print("testing Github")
+function love.load()
+
+    player = {} -- creates a Player
+    -- place player
+    player.x = 50
+    player.y = 50
+    player.speed = 2 -- decides player speed
+    
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    player = love.graphics.circle("fill", 10, 10, 10)
+end
